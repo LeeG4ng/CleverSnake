fa682bf0e962f48edef8baf5b38a7b1b21c53540
李刚 <LeeG4ng@qq.com>
2017-12-16 20:44:53 +0800

    Initial Commit

---
+++ CleverSnake/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CleverSnake
+//
+//  Created by Gary Lee on 2017/12/16.
+//  Copyright © 2017年 UniqueStudio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
